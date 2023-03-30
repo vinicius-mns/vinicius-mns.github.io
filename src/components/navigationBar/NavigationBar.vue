e8db2b386d58d88f9100a4de1bc4489753b1dee2
vinicius silva <vinicius_dev@hotmail.com>
2023-03-30 19:44:25 -0300

    feat: Cria component navigation bar para viewport desktop e tablet

---
+++ src/components/navigationBar/NavigationBar.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+
+const mockLinks = [
+  'Quem sou eu',
+  'O que eu sei fazer',
+  'Meus projetos',
+  'Explicando portifolio',
+  'Mais alguma coisa',
+  'Outro coisa',
+  'Feedbacks',
+  'Outro link',
+]
+</script>
+
+<template>
+  <div class="side-bar">
+    <nav>
+      <ul>
+        <li v-for="(link, i) of mockLinks" :key="i">
+          <RouterLink to="/"><p>{{ link }}</p></RouterLink>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.side-bar {
+  width: 98%;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  & nav {
+    width: 100%;
+    & ul {
+      width: 100%;
+      height: 25rem;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-around;
+      text-align: end;
+      list-style: none;
+    }
+  }
+}
+</style>
