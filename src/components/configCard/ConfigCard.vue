7deaa7bb6a724cf13ec096a6f1e1b56955e8132e
vinicius silva <vinicius_dev@hotmail.com>
2023-03-31 00:16:25 -0300

    feat: Cria Component (modal) card para configuracao da pagina

--- src/components/configCard/ConfigCard.vue
+++ src/components/configCard/ConfigCard.vue
@@ -1,66 +1,45 @@
 <script setup lang="ts">
+import { useGlobalStyle } from '@/stores/globalStyle'
+import { useToggleCard } from '@/stores/toddleCards';
+const style = useGlobalStyle()
+const toggle = useToggleCard()
 </script>
 
 <template>
-  <div class="container neo-card">
-    <button class="close-card"><div class="a"></div><div class="b"></div></button>
-    <div class="sub-container">
+  <div class="container" :style="style.cardStyle" v-if="toggle.settings">
+    <div class="sub-card title">
       <h2>Configuração</h2>
     </div>
-    <div class="sub-container">
-      <div><h3>Cores e estilo</h3></div>
+    <div class="sub-card config-style">
+
     </div>
-    <div class="sub-container">
-      <h3>contato</h3>
+    <div>
+
     </div>
   </div>
 </template>
 
 <style scoped lang="scss">
-  .container {
-    position: fixed;
-    width: 600px;
-    max-width: 94%;
-    min-height: 300px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+.container {
+  position: fixed;
+  width: 600px;
+  max-width: 94%;
+  min-height: 300px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
 
-    & div {
-      background-color: transparent;
-    }
-
-    & .close-card {
-      width: 2rem;
-      height: 2rem;
-      position: absolute;
-      right: 0;
-      background-color: transparent;
-      border: none;
-
-      & div {
-        position: absolute;
-        width: 100%;
-        height: 0.14rem;
-        border-radius: 2px;
-        background-color: black;
-      }
-      & .a {
-        transform: rotate(45deg);
-      }
-      & .b {
-        transform: rotate(-45deg);
-      }
-    }
-
-    & .sub-container {
-      width: 100%;
-      display: flex;
-      justify-content: center;
-    }
+  & .sub-card {
+    width: 90%;
   }
+  & .title {
+    background-color: red;
+    display: flex;
+    justify-content: center;
+  }
+}
 </style>
