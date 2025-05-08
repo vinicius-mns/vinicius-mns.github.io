d6d5c907a21932820978221a64199248a64540f9
vinicius silva <vinicius_dev@hotmail.com>
2025-05-08 00:42:04 -0300

    feat: remover ConfigTemplate 	deleted:    src/components/template/ConfigTemplate.vue 	modified:   src/components/views/ConfigPage.vue

--- src/components/views/ConfigPage.vue
+++ src/components/views/ConfigPage.vue
@@ -1,10 +1,47 @@
 <script setup lang="ts">
-import ConfigTemplate from '@/components/template/ConfigTemplate.vue'
+import CheckboxBase from '@/components/atoms/CheckboxBase.vue'
+import { useGlobalState } from '@/stores/globalState'
+import FloatRange from '@/components/molecules/FloatRange.vue'
+import { onMounted, onUpdated } from 'vue'
+
+const { trackBall, darkMode, swing, opacity, mobile } = useGlobalState()
 </script>
 
 <template>
   <div class="config-page">
-    <ConfigTemplate />
+    <!-- <ConfigTemplate /> -->
+    <div class="config-options-container">
+      <CheckboxBase
+        class="option"
+        :check="darkMode.status.dark"
+        content="Modo escuro"
+        @emit-check="darkMode.metod.darkMode($event)"
+      />
+
+      <FloatRange
+        class="option"
+        :value="opacity.status.opacity"
+        name="Opacidade"
+        :limit="{ min: 0, max: 100 }"
+        @emit-value="opacity.metod.setOpacity($event)"
+      />
+
+      <CheckboxBase
+        v-if="!mobile.status.isMobileSize"
+        class="option"
+        :check="trackBall.status.show"
+        content="Track ball"
+        @emit-check="trackBall.metod.show($event)"
+      />
+
+      <CheckboxBase
+        v-if="!mobile.status.isMobileSize"
+        class="option"
+        :check="swing.status.swing"
+        content="Balanço"
+        @emit-check="swing.metod.swingMode($event)"
+      />
+    </div>
   </div>
 </template>
 
@@ -17,5 +54,15 @@ import ConfigTemplate from '@/components/template/ConfigTemplate.vue'
   display: flex;
   align-items: end;
   justify-content: end;
+
+  & .config-options-container {
+    display: flex;
+    flex-direction: column;
+
+    & .option {
+      margin: 0;
+      padding: 10px;
+    }
+  }
 }
 </style>
